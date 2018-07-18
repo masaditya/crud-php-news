d04126574e256e9ebcea2014a2613a882e807508
Aditya Eka <33042694+masaditya@users.noreply.github.com>
2018-07-18 23:00:04 +0700

    Update indexadmin.php

--- admin/indexadmin.php
+++ admin/indexadmin.php
@@ -44,17 +44,17 @@ include("koneksi.php");
 include("list.php");
 ?>
 <?php
-$query = mysqli_query("SELECT * FROM 'tbl_berita' ORDER BY 'id' DESC");
-while ($data=mysqli_fetch_array($query)) {
-  echo '
-  <div style="max-width:450px;">
-  <hr>
-  <h3>'.$data['judul'].'</h3>
-  <small>Tanggal  : '.$data['tanggal'].'</small><br>
-  <a href="edit.php?id='.$data['id'].'">Edit</a>
-  <a href="hapus.php?id='.$data['id'].'" onclick="return confirm(\'Yakin ingin menghapus?\')">Hapus</a>
-  <p>'.$data['isi'].'</p>
-  </div>
-  ';
-}
+// $query = mysqli_query("SELECT * FROM 'tbl_berita' ORDER BY 'id' DESC");
+// while ($data=mysqli_fetch_array($query)) {
+//   echo '
+//   <div style="max-width:450px;">
+//   <hr>
+//   <h3>'.$data['judul'].'</h3>
+//   <small>Tanggal  : '.$data['tanggal'].'</small><br>
+//   <a href="edit.php?id='.$data['id'].'">Edit</a>
+//   <a href="hapus.php?id='.$data['id'].'" onclick="return confirm(\'Yakin ingin menghapus?\')">Hapus</a>
+//   <p>'.$data['isi'].'</p>
+//   </div>
+//   ';
+// }
 ?>
